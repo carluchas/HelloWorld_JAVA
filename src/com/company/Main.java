4f7e85819b22ef57e815ede32143870cab132be5
Carlos Andreu Antoranz <c.andreu.antoranz@gmail.com>
2021-03-08 00:23:21 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // Write hello world!
+        System.out.println("Hello World... Now in Java!");
+    }
+}
