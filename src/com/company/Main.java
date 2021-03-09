4ca7f2de691c9e98d329cd65c1d93050cd581f0b
Carlos Andreu Antoranz <c.andreu.antoranz@gmail.com>
2021-03-09 04:07:04 +0100

    Enums

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,26 @@ package com.company;
 
 public class Main {
 
+    enum Languages {C, PYTHON, JAVA}
+
     public static void main(String[] args) {
 	// write your code here
         // Write hello world!
-        System.out.println("Hello World... Now in Java!");
+        var msg = "Hello World... Now in Java!";
+        System.out.println(msg);
+
+        System.out.println(Languages.PYTHON);
+        System.out.println(Languages.PYTHON.ordinal());
+
+        Languages[] arr = Languages.values();
+
+        for (Languages lang:arr)
+        {
+            System.out.println(lang);
+        }
+
+
+        System.out.println(Languages.valueOf("JAVA"));
+
     }
 }
