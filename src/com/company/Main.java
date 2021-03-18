6f9413b653f8b670122383174e0c49e9230e3452
Carlos Andreu Antoranz <c.andreu.antoranz@gmail.com>
2021-03-18 02:22:20 +0100

    Refactoring

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,12 @@ public class Main {
 
     enum Languages {C, PYTHON, JAVA}
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+	    // write your code here
         // Write hello world!
         var msg = "Hello World... Now in Java!";
-        System.out.println(msg);
+        identifiedLanguage(msg);
 
         System.out.println(Languages.PYTHON);
         System.out.println(Languages.PYTHON.ordinal());
@@ -18,10 +19,34 @@ public class Main {
         for (Languages lang:arr)
         {
             System.out.println(lang);
+
+            if (lang == Languages.C)
+            {
+                identifiedLanguage("Pasa C");
+            }
+            else if (lang == Languages.PYTHON)
+            {
+                identifiedLanguage("Pasa PYTHON");
+            }
+            else if (lang == Languages.JAVA)
+            {
+                identifiedLanguage("Pasa JAVA");
+            }
+
+            switch (lang)
+            {
+                case C -> identifiedLanguage("Pasa otra vez C");
+                case PYTHON -> identifiedLanguage("Pasa otra vez PYTHON");
+                case JAVA -> identifiedLanguage("Pasa otra vez JAVA");
+            }
+
         }
 
-
         System.out.println(Languages.valueOf("JAVA"));
+    }
 
+    private static void identifiedLanguage(String message)
+    {
+        System.out.println(message);
     }
 }
